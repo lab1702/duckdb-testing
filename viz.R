87f30611ce3db1973fc49116e12a58d28c2ed0b2
lab1702 <lab1702@gmail.com>
2025-01-24 19:17:54 -0500

    Add files via upload

--- viz.R
+++ viz.R
@@ -2,14 +2,19 @@ library(tidyverse)
 
 d <- read_csv("res.csv")
 
-d |>
+p0 <- d |>
   ggplot(aes(x = OS, y = ElapsedSeconds, fill = Computer)) +
   geom_col(position = "dodge")
 
-d |>
+p1 <- d |>
   ggplot(aes(x = OS, y = CPUSeconds, fill = Computer)) +
   geom_col(position = "dodge")
 
-d |>
+p2 <- d |>
   ggplot(aes(x = OS, y = AvgConcurrentThreads, fill = Computer)) +
   geom_col(position = "dodge")
+
+
+ggsave(filename = "elapsed.svg", plot = p0, width = 8, height = 4)
+ggsave(filename = "cpu.svg", plot = p1, width = 8, height = 4)
+ggsave(filename = "concurrent.svg", plot = p2, width = 8, height = 4)
